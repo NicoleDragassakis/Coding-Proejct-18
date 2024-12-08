05586013ae3f48a67de7124c9aecd4e7314cb663
Nicole Dragassakis <niki_dragasakis@icloud.com>
2024-12-08 17:53:40 -0500

    Add reusable ChartComponent

--- src/ChartComponent.jsx
+++ src/ChartComponent.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useRef } from "react";
+import { Chart } from "chart.js";
 
-function App() {
-  const [count, setCount] = useState(0)
+const ChartComponent = ({ type, data, options }) => {
+  const chartRef = useRef(null);
+  const chartInstance = useRef(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  useEffect(() => {
+    
+    if (chartInstance.current) {
+      chartInstance.current.destroy();
+    }
 
-export default App
+  
+    chartInstance.current = new Chart(chartRef.current, {
+      type: type,
+      data: data,
+      options: options,
+    });
+
+  
+    return () => {
+      if (chartInstance.current) {
+        chartInstance.current.destroy();
+      }
+    };
+  }, [type, data, options]);
+
+  return <canvas ref={chartRef}></canvas>;
+};
+
+export default ChartComponent;
+
+
+s
